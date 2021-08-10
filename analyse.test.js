8eb511e3b328467008343f3aabf440a8f8743f0c
paposeco <13892562+paposeco@users.noreply.github.com>
2021-08-10 14:17:07 +0200

    added test with array of negative numbers

--- analyse.test.js
+++ analyse.test.js
@@ -13,11 +13,20 @@ test("new object with non numbers", () => {
   expect(arrayToObject([1, "a", 3, 4, 2, 6])).toMatch("Error");
 });
 
-test("new object from example array", () => {
+test("new object array of zeros", () => {
   expect(arrayToObject([0, 0, 0])).toMatchObject({
     average: 0,
     min: 0,
     max: 0,
+    length: 3,
+  });
+});
+
+test("new object array with negative numbers", () => {
+  expect(arrayToObject([-1, 2, -4])).toMatchObject({
+    average: -1,
+    min: -4,
+    max: 2,
     length: 3,
   });
 });
